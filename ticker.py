ab87fa5eb154d8d60633fd068a323e61f38b0445
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-08-21 23:50:16 +0000

    Cleaned up code and got rid of old stuff

--- ticker.py
+++ ticker.py
@@ -20,66 +20,10 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 15600000
 
-#set the date and time format
-date_format = "%m-%d-%Y %H:%M:%S"
-bus_date_format ="%Y-%m-%dT%H:%M:%S" 
-# add command line arguments
-parser = argparse.ArgumentParser(description='Realtime statistics counter')
-
-parser.add_argument('--busStop', dest='busStop',
-		   help='your bus stop')
-parser.add_argument('--apiKey', dest='apiKey',
-                   help='your NextBus API key')
-
-args = parser.parse_args()
-
 busStops = ["52223", "52244", "53677", "50144"]
 busRoutes = ["51B", "79"]
 busList = []
 NEXT_BUS_API_BASE = "http://webservices.nextbus.com/service/publicXMLFeed?"
-
-def getBuses(b, ak):
-        """
-        Fetches crypto currency prices. 
-        If the specified currency does not exist, return None. Otherwise, return the price in USD. 
-
-        API docs: https://coinmarketcap.com/api/
-        """
-	now = datetime.now()
-	nowstr = now.strftime(date_format)
-	nowTime = datetime.strptime(nowstr, date_format)
-
-        try:
-		url = "http://api.actransit.org/transit/stops/"
-		url += b
-		url += "/predictions/?token="
-		url += ak 
-                r = requests.get(url)
-                data = json.loads(r.text)
-		routes = ""
-                for bus in data:
-                        routes += bus['RouteName'] + " - "
-			busTime = datetime.strptime(bus['PredictedDeparture'], bus_date_format)
-			diff = busTime - nowTime
-        		minutes = (diff.seconds) / 60 - 1020
-			routes += str(minutes) + " min   "
-
-		return routes
-
-                print "Cryptocurrency not found in response data"
-                return None
-
-        except Exception as e:
-                print str(e)
-                return None
-
-def updateCounter(job_func, *args, **kwargs):
-	"""
-	Updates the LED matrix with the current count. Values are fetched from the specified job func,
-	"""
-
-	ret = functools.partial(job_func, *args, **kwargs)()
-	device.show_message(ret, font=proportional(TINY_FONT))
 
 def showBuses():
 	if (len(busList) == 0):
