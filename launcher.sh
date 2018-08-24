e43d7589ca69e5110ed05c30bd4fdf3b9b0086ff
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-08-24 04:51:58 +0000

    got rid of sleep from executable

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
-sleep 10s
+
 cd /
 cd home/pi/bus-tracker
+sudo python loading.py
 sudo python ticker.py
 cd /
